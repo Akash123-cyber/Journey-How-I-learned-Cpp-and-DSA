3759fe514be3243039ac6299052d337cfaff504f
Akash Kumar Malik <75205586+Akash123-cyber@users.noreply.github.com>
2024-11-05 20:40:12 +0530

    Simple Code day 1
    This Simple Code Explores the Conditional Statements in  C++

---
+++ day1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // cout
+    // cout <<"This is Section k23RN" <<endl;
+    // cout <<"Best Section in Btech";
+    // cout <<"Name - Akash Kumar Malik" <<endl;
+    // cout <<"Class - K23RN and Roll No is 30";
+    // cout <<"Phone Number = 8394003363";
+
+    // cin 
+    // int a,b,sum;
+    // // cin >> a;
+    // // cin >> b;
+    // cout <<"give space separated input :"<<endl;
+    // cin >> a >> b; 
+    // sum = a+b;
+    // cout << "the value of sum is : " << sum;
+
+    // Program even OR odd
+    // int a;
+    // cin >> a;
+    // if (a%2==0){
+    //     cout << "Even"<<endl;
+
+    // }
+    // else {
+    //     cout <<"Odd"<<endl;
+    // }
+
+
+
+    // Calculator
+    // 
+    
+    // Program to find the factorial of the given number
+    int x;
+    cout<<"Enter the number to find its factorial"<<endl;
+    cin >> x;
+    int f=1;
+    for (int i = x; i > 0; i--)
+    {
+        f=f*i;
+    }
+    cout <<"Factorial is : " << f <<endl; 
+    
+}
